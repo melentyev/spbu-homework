8984f08e2c661de4530821e6dc3d325e8b528d61
Kirill Melentyev <melentyev.k@gmail.com>
2014-05-23 23:57:45 +0400

    add test

--- 2014/FSharp/FSharp/Tests.fs
+++ 2014/FSharp/FSharp/Tests.fs
@@ -4,22 +4,40 @@ open FsUnit
 
 [<TestFixture>]
 module Tests = 
+    let test a b = (resolveIntersections a == b) |> should equal true
     [<Test>]
-    let ``test equal sets``() =
+    let ``equal sets``() =
         let expected = Point(1.0, 1.0)
         let data = Intersect(Point(1.0, 1.0), Intersect(VerticalLine(1.0), Line(0.0, 1.0) ) )
-        (resolveIntersections data == expected) |> should equal true
-    let ``test different sets``() =
+        test data expected
+    [<Test>]
+    let ``different sets``() =
         let expected = NoPoint
         let data = Intersect(Point(1.0, 1.0), Point(2.0, 2.0)) 
-        (resolveIntersections data == expected) |> should equal true
-    let ``test complicated1``() =
-        let expected = NoPoint
-        let data = Intersect(LineSegment((-3.0, 1.0), (-1.0, 1.0)), Intersect(Line(2.0, 5.0), Line(3.0, 7.0) ) ) 
-        (resolveIntersections data == expected) |> should equal true
-    let ``test complicated2``() =
+        test data expected
+    [<Test>]
+    let ``complicated1``() =
         let expected = Point(-2.0, 1.0)
-        let data = Intersect(LineSegment((-3.0, 1.0), (-2.5, 1.0)), Intersect(Line(2.0, 5.0), Line(3.0, 7.0) ) ) 
-        (resolveIntersections data == expected) |> should equal false
+        let data = Intersect(LineSegment((-3.0, 1.0), (-1.0, 1.0)), 
+                             Intersect(Line(2.0, 5.0), Line(3.0, 7.0) ) ) 
+        test data expected
+    [<Test>]
+    let ``complicated2``() =
+        let expected = NoPoint
+        let data = Intersect(LineSegment((-3.0, 1.0), (-2.5, 1.0)), 
+                             Intersect(Line(2.0, 5.0), Line(3.0, 7.0) ) ) 
+        test data expected
+    [<Test>]
+    let ``2 segments``() =
+        let expected = LineSegment((-1.0, -1.0), (0.0, 0.0))
+        let data = Intersect(LineSegment((-3.0, -3.0), (0.0, 0.0)), 
+                             LineSegment((2.0, 2.0), (-1.0, -1.0) ) ) 
+        test data expected
+    [<Test>]
+    let ``2 segments reversed`` () = 
+        let expected = LineSegment((0.0, 0.0), (-1.0, -1.0))
+        let data = Intersect(LineSegment((-3.0, -3.0), (0.0, 0.0)), 
+                             LineSegment((2.0, 2.0), (-1.0, -1.0) ) ) 
+        test data expected
 
-let a = Tests.``test equal sets``()
+let a = Tests.``2 segments reversed``()

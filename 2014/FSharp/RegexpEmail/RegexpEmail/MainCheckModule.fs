433c5dbe669d4dcbf42f066509565468f4fb8a6d
Kirill Melentyev <melentyev.k@gmail.com>
2014-10-15 19:13:42 +0400

    regexp mail initial commit

---
+++ 2014/FSharp/RegexpEmail/RegexpEmail/MainCheckModule.fs
@@ -0,0 +1,13 @@
+﻿module MainCheckModule
+
+//namespace 
+
+open System.Text.RegularExpressions
+
+let checkEmail s = 
+    let pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$" in Regex.Match(s, pattern).Success
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello, World";
+    0 
---
+++ 2014/FSharp/RegexpEmail/Tests/Library1.fs
@@ -0,0 +1,31 @@
+﻿namespace Tests
+open NUnit.Framework
+open FsUnit
+
+open MainCheckModule
+
+[<TestFixture>]
+module RegexpEmailTests =  
+    [<Test>]  
+    let ``aATb.cc``                             () = checkEmail "a@b.cc" |> should equal true
+    [<Test>]  
+    let ``victor.polozovATmail.ru``             () = checkEmail "victor.polozov@mail.ru" |> should equal true
+    [<Test>]  
+    let ``myATdomain.info``                     () = checkEmail "my@domain.info" |> should equal true
+    [<Test>]  
+    let ``_.1ATmail.com``                       () = checkEmail "_.1@mail.com" |> should equal true
+    [<Test>]  
+    let ``paints_departmentAThermitage.museum`` () = checkEmail "paints_department@hermitage.museum" |> should equal true
+    
+    [<Test>]  
+    let ``aATb.c``                              () = checkEmail "a@b.c" |> should equal false
+    [<Test>]  
+    let ``a..bATmail.ru``                       () = checkEmail "a..b@mail.ru" |> should equal false
+    [<Test>]  
+    let ``.aATmail.ru``                         () = checkEmail ".a@mail.ru" |> should equal false
+    [<Test>]  
+    let ``yoATdomain.somedomain``               () = checkEmail "yo@domain.somedomain" |> should equal false
+    [<Test>]  
+    let ``1ATmail.ru``                          () = checkEmail "1@mail.ru" |> should equal false
+
+

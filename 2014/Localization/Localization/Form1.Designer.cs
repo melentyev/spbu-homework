c7ddd07d813541f85de1a6373dc4d88e3b96ebf9
Kirill Melentyev <melentyev.k@gmail.com>
2015-05-26 15:06:16 +0300

    work..

--- 2014/Localization/Localization/Form1.Designer.cs
+++ 2014/Localization/Localization/Form1.Designer.cs
@@ -36,6 +36,10 @@
             this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.russianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -89,10 +93,35 @@
             resources.ApplyResources(this.russianToolStripMenuItem, "russianToolStripMenuItem");
             this.russianToolStripMenuItem.Click += new System.EventHandler(this.russianToolStripMenuItem_Click);
             // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -113,6 +142,10 @@
         private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem russianToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- 2014/Localization/Localization/Form1.cs
+++ 2014/Localization/Localization/Form1.cs
@@ -19,6 +19,7 @@ namespace Localization
         {
             InitializeComponent();
             ChangeLanguage("en-US");
+            
         }
 
         private void englishToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,17 +32,45 @@ namespace Localization
         }
         private void ChangeLanguage(string lang)
         {
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
-            foreach (Control c in this.Controls)
+            russianToolStripMenuItem.Checked = (lang == "ru-RU");
+            englishToolStripMenuItem.Checked = (lang != "ru-RU");
+            CultureInfo cult = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = cult;
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+            _ChangeLanguage(this, resources, cult);
+            _ChangeMenuStripLanguage(menuStrip1.Items, resources, cult);
+            
+        }
+        private void _ChangeLanguage(Control c, ComponentResourceManager resources, CultureInfo cult)
+        {
+            resources.ApplyResources(c, c.Name, cult);
+            foreach (Control ch in c.Controls)
             {
-                ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
-                resources.ApplyResources(c, c.Name, new CultureInfo(lang));
+                _ChangeLanguage(ch, resources, cult);
+            }
+        }
+
+        private void _ChangeMenuStripLanguage(ToolStripItemCollection collection, ComponentResourceManager resources, CultureInfo cult)
+        {
+            foreach (ToolStripItem m in collection)
+            {
+                resources.ApplyResources(m, m.Name, cult);
+                var item = m as ToolStripDropDownItem;
+                if (item != null) 
+                { 
+                    _ChangeMenuStripLanguage(item.DropDownItems, resources, cult);
+                }
             }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
